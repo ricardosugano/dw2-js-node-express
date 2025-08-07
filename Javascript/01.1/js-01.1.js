502242c78080163069ea137c1fc052f488606f59
ricardosugano <sugano.ricardo@gmail.com>
2025-08-07 19:21:34 -0300

    @ricardosugano

--- Javascript/01.1/js-01.1.js
+++ Javascript/01.1/js-01.1.js
@@ -1,9 +1,35 @@
+// VARIÁVEIS PODEM SER DECLARADAS DE 3 FORMAS:
+// VAR, LET e CONST
+// VAR: No geral evite o seu uso, pode não ser muito seguro.
+// LET: Utilize quando for necessário reatribuir o valor da variável.
+// CONST: Utilize quando não precisar reatribuir o valor da variável.
+
+// var nome = "Diego"
+// var nome = "Maria"
+// let cidade = "Registro"
+// let cidade = "Pariquera" // Não pode
+// let endereco
+// endereco = "Rua tal..."
+// const idade // Não pode
+// const idade = 18
+// idade = 20 // Não pode
+
 //TIPOS DE FUNÇÕES
 
 //FUNÇÃO SIMPLES
+const message = "<h2> Olá! Bem-vindo! Essa á a sua primeira função!</h2>";
 function showMessage() {
-    document.write("<h2> Olá! Bem-vindo! Essa á a sua primeira função!</h2>")
-
+  document.write(message);
 }
 // Invocando a função:
-showMessage()
+showMessage();
+
+// FUNÇÃO COM PARÂMETROS
+const user = "Ricardo";
+
+function userMessage(user) {
+// Essa função recebe um parâmetro
+document.write(`<h3>O que deseja fazer hoje, ${user}? <h3>`);
+}
+userMessage(user);
+
