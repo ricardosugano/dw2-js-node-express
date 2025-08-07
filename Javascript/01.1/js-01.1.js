7db9ff3c2d22d154992971aff1722ebd3f369883
ricardosugano <sugano.ricardo@gmail.com>
2025-08-06 21:05:45 -0300

    @ricardo

---
+++ Javascript/01.1/js-01.1.js
@@ -0,0 +1,9 @@
+//TIPOS DE FUNÇÕES
+
+//FUNÇÃO SIMPLES
+function showMessage() {
+    document.write("<h2> Olá! Bem-vindo! Essa á a sua primeira função!</h2>")
+
+}
+// Invocando a função:
+showMessage()
